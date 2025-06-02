8002d23da1e6e06d78af4bb1fa035eff59b15ad7
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-02 23:35:16 +0100

    feat: configure the fastify error handler

--- server/src/fastify.ts
+++ server/src/fastify.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { AppError } from './errors/errors';
 
 const fastify = Fastify({
     logger: {
@@ -21,6 +22,15 @@ fastify.register(cors, {
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+});
+
+fastify.setErrorHandler((error, request, reply) => {
+    if (error instanceof AppError) {
+        reply.status(error.statusCode).send({ error: error.message });
+    } else {
+        fastify.log.error(error);
+        reply.status(500).send({ error: 'Internal Server Error' });
+    }
 });
 
 export default fastify;
