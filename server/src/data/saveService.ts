8434d7811ca573b506be41d963ed8436ced43eba
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-03 19:05:20 +0100

    feat: create a function to get the path where the save will be stored

--- server/src/data/saveService.ts
+++ server/src/data/saveService.ts
@@ -18,6 +18,20 @@ class SaveService {
             throw new AppError("Failed to obtain the path to default database.", 500);
         }
     }
+
+    private static getSaveBasePath(): string {
+        try {
+            const documents = process.env.DOCUMENTS || path.join(os.homedir(), "Documents");
+            return path.resolve(documents, "ProPlay Games", "The Manager 2025", "games");
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                fastify.log.error("Failure to resolve the save base path:", err.message);
+            } else {
+                fastify.log.error("Unknown error resolving the save base path");
+            }
+            throw new AppError("Failed to obtain the save base path.", 500);
+        }
+    }
 }
 
 export default SaveService;
