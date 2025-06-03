7f72e92798bd7a0413b4d0a706b972defd941765
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-03 18:57:11 +0100

    feat: create a function to execute the migrations

--- server/src/data/database.ts
+++ server/src/data/database.ts
@@ -1,5 +1,7 @@
+import fs from "fs";
 import path from "path";
 import fastify from "../fastify";
+import Database from "better-sqlite3";
 import { AppError } from "../errors/errors";
 
 class DatabaseService {
@@ -14,6 +16,33 @@ class DatabaseService {
             }
 
             throw new AppError("Failed to obtain the path to migrations.", 500);
+        }
+    }
+
+    private static executeMigrations(database: Database.Database): void {
+        try {
+            const migrationsPath = this.getMigrationsPath();
+            if (!fs.existsSync(migrationsPath)) throw new AppError("Migrations folder not found", 500);
+
+            const migrationsFiles = fs.readdirSync(migrationsPath).filter(file => file.endsWith(".sql")).sort();
+
+            for (const file of migrationsFiles) {
+                const filePath = path.join(migrationsPath, file);
+                const sqlFile = fs.readFileSync(filePath, "utf-8");
+
+                database.exec(sqlFile);
+            }
+        } catch (err: unknown) {
+            if (err instanceof AppError) {
+                fastify.log.error(`Migration error: ${err.message}`);
+                throw err;
+            } else if (err instanceof Error) {
+                fastify.log.error(`Unexpected migration error: ${err.message}`);
+                throw new AppError("Unexpected error during migrations.", 500);
+            } else {
+                fastify.log.error("Unknown error during migrations");
+                throw new AppError("Unknown error during migrations.", 500);
+            }
         }
     }
 }
