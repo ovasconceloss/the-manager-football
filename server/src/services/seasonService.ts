8f62c06aa9d7df224df174181f3040c811e6d3f3
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-05 22:09:37 +0100

    feat: create the service for the seasons (before the routes and the controller)

---
+++ server/src/services/seasonService.ts
@@ -0,0 +1,21 @@
+import SeasonModel from "../models/seasonModel";
+import GameLoaderService from "../core/gameLoader";
+import FixtureService from "../core/engine/fixturesService";
+
+class SeasonService {
+    public static async fetchCurrentSeason() {
+        return await SeasonModel.getCurrentSeason();
+    }
+
+    public static async insertNewSeason(start_date: string, end_date: string) {
+        const databaseInstance = GameLoaderService.getCurrentDatabase();
+        const seasonId = await SeasonModel.insertNewSeason(start_date, end_date);
+
+        FixtureService.createFixtures(databaseInstance, seasonId as number);
+        databaseInstance.prepare(`INSERT INTO game_state (current_date, season_id) VALUES (?, ?)`).run("2025-08-01", seasonId);
+
+        return seasonId;
+    }
+}
+
+export default SeasonService;
