5cc6ab147236350e8d4bb6fdefcfe63727464164
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-08 18:01:29 +0100

    fix: correct manager routes (change post to get)

--- server/src/routes/managerRoutes.ts
+++ server/src/routes/managerRoutes.ts
@@ -2,9 +2,9 @@ import { FastifyInstance } from "fastify";
 import ManagerController from "../controllers/managerController";
 
 export default async function managerRoutes(fastify: FastifyInstance) {
+    fastify.get("/manager/details", ManagerController.getUserManagerDetails);
     fastify.post("/manager/insert", ManagerController.insertAndAssignManager);
-    fastify.post("/manager/details", ManagerController.getUserManagerDetails);
 
-    fastify.post("/manager/clubs/availables", ManagerController.getAvailableClubs);
-    fastify.post("/manager/tatical-styles", ManagerController.getTacticalStyleTypes);
+    fastify.get("/manager/clubs/availables", ManagerController.getAvailableClubs);
+    fastify.get("/manager/tatical-styles", ManagerController.getTacticalStyleTypes);
 }
