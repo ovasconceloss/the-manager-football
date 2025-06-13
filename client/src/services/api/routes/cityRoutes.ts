9058ed91b91a280f31295c31e32485d12a23bf23
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-13 17:45:28 +0100

    chore: remove the log from city routes

--- client/src/services/api/routes/cityRoutes.ts
+++ client/src/services/api/routes/cityRoutes.ts
@@ -4,7 +4,6 @@ import axiosClient from "../config/axiosConfig";
 export async function getCitiesByNation(nationId: number): Promise<{ success: boolean; response?: { data: { cities: City[] } }; error?: string }> {
     try {
         const response = await axiosClient.get(`/nation/cities/${nationId}`);
-        console.log(response);
         return { success: true, response: { data: { cities: response.data.nation } } };
     } catch (error) {
         return { success: false, error: "Failed to fetch cities" };
