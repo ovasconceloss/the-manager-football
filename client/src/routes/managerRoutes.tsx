e12daea123a4f8425c5792d3c624a1c82aa605b7
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-13 16:25:14 +0100

    feat: add a route to manager details

--- client/src/routes/managerRoutes.tsx
+++ client/src/routes/managerRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import ChooseClub from "@/screens/Managers/ChooseClub";
+import ManagerDetails from "@/screens/Managers/ManagerDetails";
 
 const ManagerRoutes: React.FC = () => {
     return (
         <Routes>
             <Route path="/choose-club" element={<ChooseClub />} />
+            <Route path="/manager-details" element={<ManagerDetails />} />
         </Routes>
     );
 };
