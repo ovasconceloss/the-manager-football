fe7d496a55a658bd76e2ee7800e0b39c366fb611
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-06-13 14:03:56 +0100

    feat: create types (typescript) for back-end data (club, league and nation

---
+++ client/src/types/club.d.ts
@@ -0,0 +1,15 @@
+export type Club = {
+    club_id: number;
+    city_id: number;
+    nation_id: number;
+    city_name: string;
+    club_name: string;
+    stadium_id: number;
+    reputation: number;
+    logo_image: string;
+    nation_name: string;
+    abbreviation: string;
+    stadium_name: string;
+    foundation_year: number;
+    stadium_capacity: number;
+}
---
+++ client/src/types/nation.d.ts
@@ -0,0 +1,9 @@
+export interface Nation {
+    id: number;
+    name: string;
+    reputation: number;
+    flag_image: string;
+    federation_name: string;
+    federation_image: string;
+    confederation_id: number;
+}
